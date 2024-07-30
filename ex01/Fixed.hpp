0ee82ebb38964a47269efe3105df5d590d31b72a
Hasina3159 <ntodisoa@student.42antananarivo.mg>
2024-07-30 15:52:00 +0300

    ex01 finished

---
+++ ex01/Fixed.hpp
@@ -0,0 +1,31 @@
+#ifndef FIXED_HPP
+# define FIXED_HPP
+
+# include <iostream>
+# include <string>
+# include <cmath>
+
+
+class Fixed
+{
+private:
+	int					m_value;
+	static const int	m_bits = 8;
+public:
+	Fixed();
+	Fixed(const Fixed &other);
+	Fixed(const int value);
+	Fixed(const float value);
+	Fixed& operator=(const Fixed &other);
+	friend std::ostream& operator<<(std::ostream &os, const Fixed &other);
+	~Fixed();
+	int getRawBits( void );
+	void setRawBits( int const raw );
+	float toFloat( void ) const;
+	int toInt( void ) const;
+
+};
+
+void    ft_print(std::string text);
+
+#endif
